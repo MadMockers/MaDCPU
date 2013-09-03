b6dd506c68f95fedaf3605df627908aec255fb06
Glen Chatfield <glenchatfield@xegup.com>
2013-09-03 23:13:57 +1000

    added special sauce

---
+++ tramp.asm
@@ -0,0 +1,20 @@
+
+; This makes a polymorphic section - code here can be written to.
+
+use64
+
+global dcpuDoBasicOperation
+global dcpuDoSpecialOperation
+global dcpuNextBasicJump
+global dcpuNextSpecialJump
+section poly progbits alloc exec write align=16
+dcpuDoBasicOperation:
+db 0xe9         ; JMP opcode
+dcpuNextBasicJump:
+dd 0
+
+dcpuDoSpecialOperation:
+db 0xe9         ; JMP opcode
+dcpuNextSpecialJump:
+dd 0
+
